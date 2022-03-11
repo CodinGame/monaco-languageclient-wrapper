8aee3351a1960a55ad8b0cfc900a05318e5a621d
Loïc Mangeonjean <loic@codingame.com>
2022-03-11 17:49:19 +0100

    feat: Implement CodinGame infrastructure

--- src/infrastructure.ts
+++ src/infrastructure.ts
@@ -46,3 +46,79 @@ export interface Infrastructure {
    */
   openConnection (id: LanguageClientId): Promise<MessageConnection>
 }
+
+function openWebsocketConnection (url: URL | string): Promise<MessageConnection> {
+  return new Promise<MessageConnection>((resolve, reject) => {
+    const webSocket = new WebSocket(url)
+
+    webSocket.onopen = () => {
+      const socket = toSocket(webSocket)
+      const webSocketConnection = createWebSocketConnection(socket, new ConsoleLogger())
+      webSocketConnection.onDispose(() => {
+        webSocket.close()
+      })
+
+      resolve(webSocketConnection)
+    }
+
+    webSocket.onerror = () => {
+      reject(new Error('Unable to connect to server'))
+    }
+  })
+}
+
+export abstract class CodinGameInfrastructure implements Infrastructure {
+  constructor (
+    /**
+     * The domain of the server
+     */
+    public serverAddress: string,
+    public useMutualizedProxy: boolean,
+    /**
+     * An optional sessionId when connecting to the session-mutualized server
+     */
+    private sessionId?: string,
+    /**
+     * A list of urls which link to zip files containing libraries/resources
+     */
+     private libraryUrls?: string[]
+  ) {
+  }
+
+  public readonly automaticTextDocumentUpdate = false
+  public readonly rootUri = 'file:///tmp/project'
+  public readonly workspaceFolders: typeof vscode.workspace.workspaceFolders = [{
+    uri: monaco.Uri.file('/tmp/project'),
+    index: 0,
+    name: 'main'
+  }]
+
+  public async saveFileContent (document: TextDocument, reason: TextDocumentSaveReason, languageClient: LanguageClientManager): Promise<void> {
+    if (languageClient.isReady()) {
+      await updateFile(document.uri.toString(), document.getText(), languageClient)
+    }
+  }
+
+  public async getFileContent (resource: monaco.Uri, languageClient: LanguageClientManager): Promise<string | null> {
+    try {
+      return (await getFile(resource.toString(true), languageClient)).text
+    } catch (error) {
+      console.error('File not found', resource.toString())
+      return null
+    }
+  }
+
+  /**
+   * A function which returns a valid JWT token to use to connect to the server
+   */
+  protected abstract getSecurityToken(): Promise<string>
+
+  public async openConnection (id: LanguageClientId): Promise<MessageConnection> {
+    const url = new URL(this.sessionId != null ? `run/${this.sessionId}/${id}` : `run/${id}`, this.serverAddress)
+    this.libraryUrls?.forEach(libraryUrl => url.searchParams.append('libraryUrl', libraryUrl))
+    url.searchParams.append('token', await this.getSecurityToken())
+
+    const connection = await openWebsocketConnection(url)
+    return connection
+  }
+}
