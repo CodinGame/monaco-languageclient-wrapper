3ea9b0d50504aab8168cbd9b5d61234c023a5614
Loïc Mangeonjean <loic@codingame.com>
2022-07-18 12:33:12 +0200

    fix: Make omnisharp metadata work, prevent error

---
+++ src/extensions/csharp.ts
@@ -0,0 +1,38 @@
+import { MonacoLanguageClient, ProtocolRequestType } from 'monaco-languageclient'
+import { Uri, ExtensionContext, workspace, CancellationToken } from 'vscode'
+import { ExtensionFeature } from './tools'
+
+const OmnisharpMetadataRequestType = new ProtocolRequestType<{
+  Timeout: number
+  AssemblyName: string
+  Language: string | null
+  ProjectName: string
+  TypeName: string
+  VersionNumber: string | null
+}, {
+  Source: string
+  SourceName: string
+}, never, void, void>('o#/metadata')
+
+export class CsharpExtensionFeature extends ExtensionFeature {
+  constructor (private languageClient: MonacoLanguageClient) {
+    super()
+  }
+
+  activate (context: ExtensionContext): void {
+    context.subscriptions.push(workspace.registerTextDocumentContentProvider('omnisharp-metadata', {
+      provideTextDocumentContent: async (uri: Uri, token: CancellationToken): Promise<string> => {
+        const [, project, assembly, typeName] = /^\/Project\/(.*)\/Assembly\/(.*)\/Symbol\/(.*).cs$/.exec(uri.fsPath)!
+        const { Source } = await this.languageClient.sendRequest(OmnisharpMetadataRequestType, {
+          Timeout: 5000,
+          AssemblyName: assembly!.replace(/\//g, '.'),
+          Language: null,
+          ProjectName: project!,
+          TypeName: typeName!.replace(/\//g, '.'),
+          VersionNumber: null
+        }, token)
+        return Source
+      }
+    }))
+  }
+}
