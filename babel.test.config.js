b74a9266af1e47e4593a388e6ad397bae187fd65
Loïc Mangeonjean <loic@coderpad.io>
2024-01-29 12:37:08 +0100

    fix: fix tests

--- babel.test.config.js
+++ babel.test.config.js
@@ -2,7 +2,8 @@ module.exports = {
   plugins: [
     ["@babel/plugin-transform-modules-commonjs", {
       importInterop: 'babel'
-    }]
+    }],
+    "babel-plugin-transform-import-meta"
   ],
   presets: [
     "@babel/preset-env",
